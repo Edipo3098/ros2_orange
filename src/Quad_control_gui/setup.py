e4f567a57df951e9e718125ff86a713cbcd789b1
Edinson Porras <felipe.porras.014@gmail.com>
2024-09-17 20:43:42 +0200

    Add initial GUI with qt5 and ROS2 node to connect to matlab and MPU publisher node

---
+++ src/Quad_control_gui/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'Quad_control_gui'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='edipo',
+    maintainer_email='felipe.porras.014@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'control_gui = Quad_control_gui.control_gui:main',  # ROS 2 executable exceutable = package_name + file_name + function_name
+        ],
+    },
+)
