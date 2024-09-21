0b4c6afc133ec6706dd807cfabe1c20334d0f32f
Edinson Porras <felipe.porras.014@gmail.com>
2024-09-22 00:02:31 +0200

    Know is always zero

--- src/mpu_pub/mpu_pub/mpu_publisher.py
+++ src/mpu_pub/mpu_pub/mpu_publisher.py
@@ -299,10 +299,11 @@ class MinimalPublisher(Node):
                     # Apply adaptive calibration to adjust offsets in real-time
                     accel_data_filtered.append([accel_x, accel_y, accel_z])
                     
-                    self.get_logger().info(f"Not finish Accel_x: {accel_x}, Accel_y: {accel_y}, Accel_z: {accel_z}")
+                    
 
                     if len(accel_data_filtered) >= num_samples:
                         self.adaptive_calibration([accel_x, accel_y, accel_z], key)
+                        self.get_logger().info(f"Not finish Accel_x: {accel_x}, Accel_y: {accel_y}, Accel_z: {accel_z}")
                         if (accel_data_filtered < 0.1).all():
                             finishCalibration = True
 
