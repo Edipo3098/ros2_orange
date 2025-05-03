00505c8e57959f2fb1080d03d8041b294fbe510c
Edinson Porras <felipe.porras.014@gmail.com>
2025-05-03 17:59:08 +0200

    Improve serial communication

--- src/mpu_pub/mpu_pub/motor_subscriber.py
+++ src/mpu_pub/mpu_pub/motor_subscriber.py
@@ -33,6 +33,7 @@ class MinimalSubscriber(Node):
         self.timer_communication = self.create_timer(1, self.checkCommunication_Arduino)
         self.Sending = False
         self.tryng_coommunication = 0
+        self.msg_move = MoveRobot()
         
         #self.checkCommunication_Arduino()
 
@@ -45,6 +46,7 @@ class MinimalSubscriber(Node):
         self.msg_command.quadmoving = False
 
         self.publishers_.publish(self.msg_command)
+        self.sendCommand = False
         self.isARM  = False
         self.isGait = False
         self.get_logger().info('Publish true')
@@ -57,6 +59,37 @@ class MinimalSubscriber(Node):
         ser = serial.Serial(serial_port, baud_rate, timeout=1)
         received_data = ser.readline().decode().strip()
         if self.Sending:
+            if self.sendCommand:
+                if ( self.isARM):
+                    self.get_logger().info('moving: ARM ')
+                    ser.write(str(self.msg_move.command).encode())
+                    ser.write(B"\n")
+                    time.sleep(0.2)
+                    csv_line = f"{self.msg_move.m0},{self.msg_move.m1},{self.msg_move.m2},{self.msg_move.m3},{self.msg_move.m4},{2}\n"
+                    ser.write(csv_line.encode()) 
+                    self.isARM = False
+                    self.msg_command.armmoving = True
+                    self.msg_command.grippermoving = False
+                    self.msg_command.gripperopen = False
+                    self.msg_command.gripperclosed = False
+                    self.msg_command.quadmoving = False
+                elif (self.isGait):
+                    self.get_logger().info('moving: GAIT ')
+                    ser.write(str(self.msg_move.command).encode())
+                    ser.write(B"\n")
+                    self.isGait = False
+                    self.msg_command.armmoving = False
+                    self.msg_command.grippermoving = False
+                    self.msg_command.gripperopen = False
+                    self.msg_command.gripperclosed = False
+                    self.msg_command.quadmoving = True
+                    
+                else:
+                    self.get_logger().info('moving: ORIGIN ')
+                    ser.write(str(self.msg_move.command).encode())
+                    ser.write(B"\n")
+                    self.isARM = False 
+                self.sendCommand = False 
             if received_data == "move":
                     self.get_logger().info('moving: "%s"' % received_data)
             if received_data == "FL":
@@ -72,7 +105,7 @@ class MinimalSubscriber(Node):
                 self.get_logger().info('Received: "%s"' % received_data)
 
                 self.msg_command.ready = True
-                ser.close()
+                
                 self.Sending = False
             else:
 
@@ -85,10 +118,11 @@ class MinimalSubscriber(Node):
             self.msg_command.gripperclosed = False
             self.msg_command.quadmoving = False
             self.publishers_.publish(self.msg_command )
+            ser.close()
             return
        
 
-        # Send data over the serial connection
+        # Send data over the serial connection IF NOT COMMAND
         ser.write(str('check').encode())
         ser.write(B"\n")
         time.sleep(0.2)
@@ -116,6 +150,7 @@ class MinimalSubscriber(Node):
         
   
     def listener_callback(self, msg):
+        self.sendCommand = True
         self.Sending = True
         self.get_logger().info('Command "%s"' % msg.command)
         if msg.command == "ARM":
@@ -132,57 +167,9 @@ class MinimalSubscriber(Node):
             self.get_logger().info('is stoping gait')
             
             self.isARM = False
-        serial_port = '/dev/ttyS5'
-        baud_rate = 115200
-
-        ser = serial.Serial(serial_port, baud_rate, timeout=1)
-
-        counter = 0
-        try:
-            # Send data over the serial connection
-            
-            
-            if ( self.isARM):
-                ser.write(str(msg.command).encode())
-                ser.write(B"\n")
-                time.sleep(0.2)
-                csv_line = f"{msg.m0},{msg.m1},{msg.m2},{msg.m3},{msg.m4},{2}\n"
-                ser.write(csv_line.encode()) 
-                self.isARM = False
-                self.msg_command.armmoving = True
-                self.msg_command.grippermoving = False
-                self.msg_command.gripperopen = False
-                self.msg_command.gripperclosed = False
-                self.msg_command.quadmoving = False
-            elif (self.isGait):
-                ser.write(str(msg.command).encode())
-                ser.write(B"\n")
-                self.isGait = False
-                self.msg_command.armmoving = False
-                self.msg_command.grippermoving = False
-                self.msg_command.gripperopen = False
-                self.msg_command.gripperclosed = False
-                self.msg_command.quadmoving = True
-                
-            else:
-                ser.write(str(msg.command).encode())
-                ser.write(B"\n")
-                self.isARM = False    
-            # Wait for a moment
-            
-            # Read response from the serial connection
-            self.msg_command.ready = False
-            self.publishers_.publish(self.msg_command )
-            received_data = "False"
-            ser.close()
-            
-        except KeyboardInterrupt:
-            # If Ctrl+C is pressed, break out of the loop
-            print("Keyboard interrupt detected. Exiting...")
-        finally:
-            # Close the serial port, even if an exception occurs
-            ser.close()
-        
+        self.msg_move  = msg
+        self.msg_command.ready = False
+           
         
         
 
