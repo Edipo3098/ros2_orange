02123ce22a878c26d3677ab33194b123603968fd
Edinson Porras <felipe.porras.014@gmail.com>
2025-05-04 20:26:24 +0200

    Try

--- src/mpu_pub/mpu_pub/motor_subscriber.py
+++ src/mpu_pub/mpu_pub/motor_subscriber.py
@@ -30,11 +30,12 @@ class MinimalSubscriber(Node):
         self.publishers_ = self.create_publisher(Command, 'command_robot', 10)
         timer_period = 0.2  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.timer_communication = self.create_timer(2, self.checkCommunication_Arduino)
+        #self.timer_communication = self.create_timer(2, self.checkCommunication_Arduino)
+        self.timer_comm = self.create_timer(0.5, self.communicacion_arduino)
         self.Sending = False
         
         #self.checkCommunication_Arduino()
-
+        self.robot_command = MoveRobot()    
         self.msg_command = Command()
         self.msg_command.ready = True
         self.msg_command.armmoving = False
@@ -46,8 +47,106 @@ class MinimalSubscriber(Node):
         self.publishers_.publish(self.msg_command)
         self.isARM  = False
         self.isGait = False
+        self.command_sended = False
+        self.received_data = "False"
         self.get_logger().info('Publish true')
-        
+    def communicacion_arduino(self):
+        msg = self.robot_command
+        self.get_logger().info('Received: "%s"' % msg)
+        self.get_logger().info('Command "%s"' % msg.command)
+        self.isARM = False
+        self.isGait = False
+        if msg.command == "ARM":
+            self.get_logger().info('ARM')
+            self.isARM = True
+            
+        elif msg.command == "m4":
+            msg.command = "m3"
+            self.get_logger().info('is publishing static gait')
+            self.isGait = True
+            
+        else:
+            msg.command = "origin"
+            self.get_logger().info('is stoping gait')
+            
+            self.isARM = False
+        serial_port = '/dev/ttyS5'
+        baud_rate = 115200
+
+        ser = serial.Serial(serial_port, baud_rate, timeout=1)
+
+        counter = 0
+        try:
+            # Send data over the serial connection
+            if not self.command_sended:
+                if ( self.isARM):
+                    ser.write(str(msg.command).encode())
+                    ser.write(B"\n")
+                
+                    csv_line = f"{int(msg.m0)},{int(msg.m1)},{int(msg.m2)},{int(msg.m3)},{int(msg.m4)},{int(msg.m5)}\n"
+                    ser.write(csv_line.encode()) 
+                    self.get_logger().info('Sended: "%s"' % csv_line)
+                    self.msg_command.armmoving = True
+                    self.msg_command.grippermoving = False
+                    self.msg_command.gripperopen = False
+                    self.msg_command.gripperclosed = False
+                    self.msg_command.quadmoving = False
+                    self.command_sended = True
+                elif (self.isGait):
+                    csv_line = f"{msg.command}\n"
+                    ser.write(csv_line.encode()) 
+                    self.command_sended = True
+                    #ser.write(str(msg.command).encode())
+                    #ser.write(B"\n")
+                    
+                    self.msg_command.armmoving = False
+                    self.msg_command.grippermoving = False
+                    self.msg_command.gripperopen = False
+                    self.msg_command.gripperclosed = False
+                    self.msg_command.quadmoving = True
+                    
+                else:
+                    ser.write(str(msg.command).encode())
+                    ser.write(B"\n")
+                    self.get_logger().info('Sended: "%s"' % msg.command)
+                    self.command_sended = True
+                 
+            # Wait for a moment
+            
+            # Read response from the serial connection
+            self.msg_command.ready = False
+            self.publishers_.publish(self.msg_command )
+            
+            if self.received_data  != "True" and self.command_sended:
+                try:
+                    self.received_data  = ser.readline().decode().strip()
+                    
+                except UnicodeDecodeError as e:
+                    self.get_logger().warn(f"Error decoding {self.received_data !r}: {e}")
+                    
+                self.get_logger().info('Waiting callback true: "%s"' % self.received_data)
+                
+                    
+                if self.received_data  == "True":
+                    self.get_logger().info('Received: "%s"' % self.received_data )
+                    self.command_sended = False
+                    self.msg_command.ready = True
+                else:
+                    
+                    self.msg_command.ready = False
+            ser.reset_input_buffer()   
+
+        except KeyboardInterrupt:
+            # If Ctrl+C is pressed, break out of the loop
+            print("Keyboard interrupt detected. Exiting...")
+        finally:
+            # Close the serial port, even if an exception occurs
+            ser.reset_input_buffer()
+            ser.close()
+            self.timer_communication.cancel()
+        self.Sending = False
+        self.timer_communication = self.create_timer(2, self.checkCommunication_Arduino)
+           
     def checkCommunication_Arduino(self):
         
         try:
@@ -96,103 +195,7 @@ class MinimalSubscriber(Node):
   
     def listener_callback(self, msg):
         self.Sending = True
-        self.timer_communication.cancel()
-        self.get_logger().info('Command "%s"' % msg.command)
-        self.isARM = False
-        self.isGait = False
-        if msg.command == "ARM":
-            self.get_logger().info('ARM')
-            self.isARM = True
-            
-        elif msg.command == "m4":
-            msg.command = "m3"
-            self.get_logger().info('is publishing static gait')
-            self.isGait = True
-            
-        else:
-            msg.command = "origin"
-            self.get_logger().info('is stoping gait')
-            
-            self.isARM = False
-        serial_port = '/dev/ttyS5'
-        baud_rate = 115200
-
-        ser = serial.Serial(serial_port, baud_rate, timeout=1)
-
-        counter = 0
-        try:
-            # Send data over the serial connection
-            
-            
-            if ( self.isARM):
-                ser.write(str(msg.command).encode())
-                ser.write(B"\n")
-               
-                csv_line = f"{int(msg.m0)},{int(msg.m1)},{int(msg.m2)},{int(msg.m3)},{int(msg.m4)},{int(msg.m5)}\n"
-                ser.write(csv_line.encode()) 
-                self.get_logger().info('Sended: "%s"' % csv_line)
-                self.msg_command.armmoving = True
-                self.msg_command.grippermoving = False
-                self.msg_command.gripperopen = False
-                self.msg_command.gripperclosed = False
-                self.msg_command.quadmoving = False
-            elif (self.isGait):
-                csv_line = f"{msg.command}\n"
-                ser.write(csv_line.encode()) 
-                #ser.write(str(msg.command).encode())
-                #ser.write(B"\n")
-                
-                self.msg_command.armmoving = False
-                self.msg_command.grippermoving = False
-                self.msg_command.gripperopen = False
-                self.msg_command.gripperclosed = False
-                self.msg_command.quadmoving = True
-                
-            else:
-                ser.write(str(msg.command).encode())
-                ser.write(B"\n")
-                 
-            # Wait for a moment
-            
-            # Read response from the serial connection
-            self.msg_command.ready = False
-            self.publishers_.publish(self.msg_command )
-            received_data = "False"
-            while received_data != "True":
-                try:
-                    received_data = ser.readline().decode().strip()
-                    
-                except UnicodeDecodeError as e:
-                    self.get_logger().warn(f"Error decoding {received_data!r}: {e}")
-                    received_data = "Check"
-                self.get_logger().info('Waiting callback true: "%s"' % received_data)
-                counter += 1
-                if counter > 25:
-                    self.get_logger().info('Done true: "%s"' % received_data)
-                    
-                    self.msg_command.ready = False
-                    ser.reset_input_buffer()
-                    ser.close()
-                    break
-                if received_data == "True":
-                    self.get_logger().info('Received: "%s"' % received_data)
-                    
-                    self.msg_command.ready = True
-                else:
-                    
-                    self.msg_command.ready = False
-                
-
-        except KeyboardInterrupt:
-            # If Ctrl+C is pressed, break out of the loop
-            print("Keyboard interrupt detected. Exiting...")
-        finally:
-            # Close the serial port, even if an exception occurs
-            ser.reset_input_buffer()
-            ser.close()
-            self.timer_communication.cancel()
-        self.Sending = False
-        self.timer_communication = self.create_timer(2, self.checkCommunication_Arduino)
+        self.robot_command = msg
         
         
 
